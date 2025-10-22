724f732cef21981349b2ec20d7066881b8ca187e
ivan2214 <bongiovanniivan12@gmail.com>
2025-10-22 13:31:17 -0300

    fix styles

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -100,17 +100,17 @@ export default async function HomePage() {
     <div className="mx-auto flex flex-col p-5">
       {/* Featured Products */}
       {featuredProducts.length > 0 && (
-        <section className="py-20">
-          <div className="container mx-auto mb-8 flex items-center justify-between">
-            <div>
+        <section className="py-5 md:py-20">
+          <div className="mb-8 flex w-full flex-col items-start gap-4 md:flex-row md:items-center md:justify-between">
+            <div className="flex flex-col items-start gap-2">
               <h2 className="font-bold text-2xl tracking-tight md:text-3xl">
                 Productos Destacados
               </h2>
-              <p className="mt-2 text-muted-foreground text-sm md:text-lg">
+              <p className="text-muted-foreground text-sm md:text-lg">
                 Descubre lo que ofrecen los comercios locales
               </p>
             </div>
-            <Button asChild variant="outline" className="bg-transparent">
+            <Button asChild>
               <Link href="/explorar">Ver Todos</Link>
             </Button>
           </div>
@@ -120,8 +120,8 @@ export default async function HomePage() {
       )}
 
       {Object.entries(productsByCategory).map(([category, products]) => (
-        <section key={category} className="py-10">
-          <div className="container mx-auto mb-8 flex items-center justify-between">
+        <section key={category} className="py-5 md:py-20">
+          <div className="mb-8 flex w-full flex-col items-start gap-4 md:flex-row md:items-center md:justify-between">
             <div>
               <h2 className="font-bold text-3xl tracking-tight">
                 {category.charAt(0).toUpperCase() + category.slice(1)}
