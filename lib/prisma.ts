3a8ea8ca8f900959218d67e35be05476022bc39b
ivan2214 <bongiovanniivan12@gmail.com>
2025-11-27 00:35:30 -0300

    chore: change minuts per miliseconds and more seconds

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,17 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../app/generated/prisma/client";
 
-const secondsPerMinute = 60;
-const minutesInLocal = 1;
-const minutesInProduction = 2;
-const secondsInLocal = minutesInLocal * secondsPerMinute;
-const secondsInProduction = minutesInProduction * secondsPerMinute;
+const milisecondsPerMinute = 60 * 1000;
+const minutesInLocal = 2;
+const minutesInProduction = 4;
+const secondsInLocal = minutesInLocal * milisecondsPerMinute;
+const secondsInProduction = minutesInProduction * milisecondsPerMinute;
 
 const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({
   adapter: pool,
   transactionOptions: {
-    // espera de 2 min en produccion y en local de 60 seg
+    // espera de 4 min en produccion y en local de 60 seg
     maxWait:
       process.env.NODE_ENV === "production"
         ? secondsInProduction
