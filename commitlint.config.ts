069a86e5d91094fd9a7a5b16b2dea4f6096efbc4
ivan2214 <bongiovanniivan12@gmail.com>
2025-12-13 22:28:40 -0300

    chore: Disable commitlint casing rules and increase body line length limit.

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,6 +5,12 @@ const Configuration: UserConfig = {
   rules: {
     "header-max-length": [0, "always", 100], // Desactiva la longitud máxima
     "subject-full-stop": [0, "never", "."], // Permite puntos al final
+    "body-max-line-length": [0, "always", 200], // Permite líneas más largas en el cuerpo
+    // 🔓 Permite mayúscula o minúscula
+    "subject-case": [0],
+    "header-case": [0],
+    "body-case": [0],
+    "type-case": [0],
   },
 };
 
