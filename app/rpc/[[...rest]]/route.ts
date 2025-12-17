0c503e8f053780da16092d218153e398df877dd9
ivan2214 <bongiovanniivan12@gmail.com>
2025-12-17 16:56:41 -0300

    fix: mutation error and router errror with documentation

--- app/rpc/[[...rest]]/route.ts
+++ app/rpc/[[...rest]]/route.ts
@@ -1,58 +1,19 @@
-import { experimental_SmartCoercionPlugin as SmartCoercionPlugin } from "@orpc/json-schema";
-import { OpenAPIHandler } from "@orpc/openapi/fetch";
-import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
 import { onError } from "@orpc/server";
-import { ZodToJsonSchemaConverter } from "@orpc/zod/zod4";
+import { RPCHandler } from "@orpc/server/fetch";
 import { router } from "@/app/router";
 
-const openAPIHandler = new OpenAPIHandler(router, {
+const handler = new RPCHandler(router, {
   interceptors: [
     onError((error) => {
       console.error(error);
     }),
   ],
-  plugins: [
-    new SmartCoercionPlugin({
-      schemaConverters: [new ZodToJsonSchemaConverter()],
-    }),
-    new OpenAPIReferencePlugin({
-      schemaConverters: [new ZodToJsonSchemaConverter()],
-      specGenerateOptions: {
-        info: {
-          title: "ORPC Playground",
-          version: "1.0.0",
-        },
-
-        commonSchemas: {
-          UndefinedError: { error: "UndefinedError" },
-        },
-        security: [{ bearerAuth: [] }],
-        components: {
-          securitySchemes: {
-            bearerAuth: {
-              type: "http",
-              scheme: "bearer",
-            },
-          },
-        },
-      },
-      docsConfig: {
-        authentication: {
-          securitySchemes: {
-            bearerAuth: {
-              token: "default-token",
-            },
-          },
-        },
-      },
-    }),
-  ],
 });
 
 async function handleRequest(request: Request) {
-  const { response } = await openAPIHandler.handle(request, {
+  const { response } = await handler.handle(request, {
     prefix: "/rpc",
-    context: {},
+    context: {}, // Provide initial context if needed
   });
 
   return response ?? new Response("Not found", { status: 404 });
