64baac9f92b41a73cd287ba267371d03d3d1a23a
ivan2214 <bongiovanniivan12@gmail.com>
2025-12-18 19:43:48 -0300

    fix: remove no used property

--- app/(public)/_components/product-list.tsx
+++ app/(public)/_components/product-list.tsx
@@ -9,7 +9,7 @@ export function ProductList({
   return (
     <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {products.map((product) => (
-        <ProductCard isCarrousel={false} key={product.id} product={product} />
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
