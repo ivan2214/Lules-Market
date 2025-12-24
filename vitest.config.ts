db4064b621ba3102c85813910afebd7182034a92
ivan2214 <bongiovanniivan12@gmail.com>
2025-12-24 12:15:00 -0300

    fix: config vitest tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,7 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: ["./vitest.setup.ts"],
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
+    exclude: ["e2e/**", "node_modules/**"],
   },
 });
